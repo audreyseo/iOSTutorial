fe395cc01373b0d4d06478f165d7d32e6e7edc89
audreyseo <audreyleeseo@gmail.com>
2017-01-11 10:51:54 -0500

    Initial Commit for Tutorial Repo
    Making a little tutorial repository.

---
+++ tutorial/Cell.swift
@@ -0,0 +1,42 @@
+//
+//  Cell.swift
+//  tutorial
+//
+//  Created by Audrey Seo on 11/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+
+class Cell:UITableViewCell {
+	
+	let nameLabel:UILabel = {
+		// Instantiate a label in a closure
+		let l = UILabel()
+		l.translatesAutoresizingMaskIntoConstraints = false
+		l.text = "Sample"
+		l.font = UIFont.systemFont(ofSize: 15)
+	}() // Run the closure
+	
+	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		// Do any other stuff that you might want down here
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("We're not using aDecoder.")
+	}
+	
+	func myConstraints(vf:String, views: [String: Any]) {
+		addConstraints(NSLayoutConstraint.constraints(withVisualFormat: vf, options: NSLayoutFormatOptions(), metrics: nil, views: views))
+	}
+	
+	func setupViews() {
+		// Do all of the adding of subviews and constraints here
+		addSubview(nameLabel)
+		
+		myConstraints(vf: "H:|-16-[v0]->=16-|", views: ["v0": nameLabel])
+	}
+}
---
+++ tutorial/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  tutorial
+//
+//  Created by Audrey Seo on 11/1/2017.
+//  Copyright © 2017 Audrey Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+	let cellId = "cellId"
+	
+	var items:[String] = [String]()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		tableView.register(Cell.self, forCellReuseIdentifier: cellId)
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let c = tableView.dequeueReusableCell(withIdentifier: cellId) as! Cell
+		c.nameLabel.text = items[indexPath.row]
+		return c
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return items.count
+	}
+}
+
