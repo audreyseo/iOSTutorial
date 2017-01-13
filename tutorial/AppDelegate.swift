2ded38f381ccd2ca3a3fcc62e83b595852dae03d
audreyseo <audreyleeseo@gmail.com>
2017-01-13 11:41:32 -0500

    Added controller for data
    Had to instantiate data somehow. Have not implemented this new class,
    DataController yet.
    
    Also added the data model, tutorialModel. DataController accesses the
    data in tutorialModel as a sqlite db.
    
    Needs to test.

--- tutorial/AppDelegate.swift
+++ tutorial/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -40,7 +41,96 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func applicationWillTerminate(_ application: UIApplication) {
 		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 	}
-
-
+//		self.saveContext()
+//	}
+//	
+//	
+//	func saveContext() {
+////		var error: NSError? = nil
+//		
+//		let managedObjectContext = self.managedObjectContext
+//		
+////		if managedObjectContext != nil {
+//		if managedObjectContext.hasChanges {
+//			do {
+//				try managedObjectContext.save()
+//			} catch {
+//				print("saveContext(): Error with saving context.")
+//				abort()
+//			}
+//		}
+////			if managedObjectContext.hasChanges && !managedObjectContext.save(&error) {
+////				// Replace this implementation with code to handle the error appropriately.
+////				// abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+////				//println("Unresolved error \(error), \(error.userInfo)")
+////
+////			}
+////		}
+//	}
+//	
+//	lazy var applicationDocumentsDirectory: NSURL = {
+//		let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//		return urls[urls.endIndex - 1] as NSURL
+//	}()
+//	
+//	lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+//		var _persistentStoreCoordinator: NSPersistentStoreCoordinator? = nil
+//		if _persistentStoreCoordinator != nil {
+//			let storeURL = self.applicationDocumentsDirectory.appendingPathComponent("[somefile].sqlite")
+////			var error: NSError? = nil
+//			_persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+//			do {
+//				try _persistentStoreCoordinator!.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: storeURL, options: nil)
+//			} catch {
+//				/*
+//				Replace this implementation with code to handle the error appropriately.
+//				abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//				Typical reasons for an error here include:
+//				* The persistent store is not accessible;
+//				* The schema for the persistent store is incompatible with current managed object model.
+//				Check the error message to determine what the actual problem was.
+//				If the persistent store is not accessible, there is typically something wrong with the file path. Often, a file URL is pointing into the application's resources directory instead of a writeable directory.
+//				If you encounter schema incompatibility errors during development, you can reduce their frequency by:
+//				* Simply deleting the existing store:
+//				NSFileManager.defaultManager().removeItemAtURL(storeURL, error: nil)
+//				* Performing automatic lightweight migration by passing the following dictionary as the options parameter:
+//				[NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true}
+//				Lightweight migration will only work for a limited set of schema changes; consult "Core Data Model Versioning and Data Migration Programming Guide" for details.
+//				*/
+//				//println("Unresolved error \(error), \(error.userInfo)")
+//				abort()
+//			}
+//			
+//		}
+//		return _persistentStoreCoordinator!
+//	}()
+//	
+//	
+//	// MARK: - Core Data stack
+//	
+//	lazy var managedObjectContext: NSManagedObjectContext = {
+//		var _managedObjectContext: NSManagedObjectContext? = nil
+//		if _managedObjectContext == nil {
+//			let coordinator = self.persistentStoreCoordinator
+//			if coordinator != nil {
+//				_managedObjectContext = NSManagedObjectContext.init(concurrencyType: NSManagedObjectContextConcurrencyType.mainQueueConcurrencyType)
+//				_managedObjectContext!.persistentStoreCoordinator = coordinator
+//			}
+//		}
+//		return _managedObjectContext!
+//	}()
+//	
+//	
+//	
+//	
+//	lazy var managedObjectModel: NSManagedObjectModel = {
+//		var _managedObjectModel: NSManagedObjectModel? = nil
+//		if _managedObjectModel != nil {
+//			let modelURL = Bundle.main.url(forResource: "[somefile]", withExtension: ".sqlite")
+//			_managedObjectModel = NSManagedObjectModel(contentsOf: modelURL!)
+//		}
+//		return _managedObjectModel!
+//	}()
+//	
 }
 
